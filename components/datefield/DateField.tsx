180bedadf908e636b94990f8515be5b093c2e122
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-07-30 21:45:10 -0300

    Dash

--- components/datefield/DateField.tsx
+++ components/datefield/DateField.tsx
@@ -20,3 +20,24 @@ const DateField: React.FC<{ selectedDate: Date; onChange: (date: Date) => void }
 };
 
 export default DateField;
+
+/* import React from 'react';
+import { format } from 'date-fns';
+
+const DateField = ({ selectedDate, onChange }) => {
+  return (
+    <div className="flex items-center">
+      <input
+        type="date"
+        value={format(selectedDate, 'yyyy-MM-dd')}
+        onChange={(e) => onChange(new Date(e.target.value))}
+      />
+      <p className="text-xs text-center opacity-75 ml-2">
+        {format(selectedDate, 'dd/MM/yyyy')}
+      </p>
+    </div>
+  );
+};
+
+export default DateField;
+ */
